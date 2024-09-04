2a758fb27b06e62a39d449a5c690dd8a423af125
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2024-09-04 21:22:13 +0530

    commit
    commit

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-
-import express from 'express';
-import dotenv from 'dotenv';
-import colors from 'colors';
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
 import connectDB from "./config/db.js";
-
+import morgan from "morgan";
 //dotenv config
-dotenv.config()
+dotenv.config();
 
 // mongodb connection
 connectDB();
 //rest object
 const app = express();
 
+// middle ware
+app.use(morgan("dev"));
 //routes
-app.get('/',(req,res) =>{
-      res.send("<h1>Welcome to Job Portal<h1>")
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Job Portal<h1>");
 });
 
-const PORT = process.env.PORT ||8080
+const PORT = process.env.PORT || 8080;
 
 //listen
-app.listen(PORT,() =>{
-    console.log(`Node Server Running In ${process.env.DEV_MODE} Mode on port no ${PORT}`.bgCyan.white);
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} Mode on port no ${PORT}`
+      .bgBlack.white
+  );
 });
