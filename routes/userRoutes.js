d039947c5db5d28763d4f3b136df28e686e9d43b
Kunal Raj Pandey <pandeykunalraj@gmail.com>
2024-09-06 20:47:16 +0530

    aa

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,7 @@ import { updateUserController } from "../controllers/userController.js";
 //router object 
 const router = express.Router();
 
-//routes
-//GET USERS || GET
 
 //UPDATE USER || PUT
-router.put('/update-user',userAuth,updateUserController) 
-export default router
+router.put('/update-user',userAuth,updateUserController);
+export default router;
