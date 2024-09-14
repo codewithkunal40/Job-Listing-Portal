ee8ce464e84651545dddc225e19794170fa47e27
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2024-09-14 21:18:08 +0530

    aa
    aa

--- client/src/pages/EmployerDashboardRoutes.js
+++ client/src/pages/EmployerDashboardRoutes.js
@@ -8,11 +8,12 @@ import Settings from "./Settings";
 import JobList from "./JobList";
 import JobStats from "./JobStats";
 import Logout from "./Logout";
+import EmployerDashboard from "./EmployerDashboard";
 
 const EmployerDashboardRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<DashboardHome />} />
+      <Route path="/" element={<EmployerDashboard />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/create-job" element={<CreateJob />} />
       <Route path="/job-list" element={<JobList />} />
