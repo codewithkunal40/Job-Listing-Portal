5bb541acc7a26f220ecfa193603b4819b789cd27
BalakrishnanShekar <ksbalu67@gmail.com>
2024-09-16 12:54:57 +0530

    aa

--- client/src/pages/JobDashboard.js
+++ client/src/pages/JobDashboard.js
@@ -23,6 +23,7 @@ const JobDashboard = () => {
             <Route path="/settings" element={<Settings />} />
             <Route path="/logout" element={<Logout />} />
             <Route path="/contact" element={<Contactform />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </div>
       </div>
