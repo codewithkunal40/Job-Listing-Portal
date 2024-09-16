9de4d1e9053293fd6921db6196107e5ef53571cc
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2024-09-16 20:17:46 +0530

    aa
    aa

--- client/src/pages/JobDashboard.js
+++ client/src/pages/JobDashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 import DashboardHome from "./DashboardHome";
 import Profile from "./Profile";
